2f2ea5c3f37ed11336b5e6281738a0724c856073
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-09-27 17:36:56 -0300

    adicionar os arquivos do projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,51 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./styles/page.module.css";
+
+import Logo from './assets/images/logo.svg'
+import Document from './assets/images/icon-document.svg'
+import Folder from './assets/images/icon-folder.svg'
+import Upload from './assets/images/icon-upload.svg'
+import BackgroundDesktop from './assets/images/bg-desktop.png'
+
+export const metadata = {
+  title: 'Componente armazenamento de dados',
+  description: 'Desafio do frontend mentor',
+  author: 'João Vitor de Souza'
+}
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main className={styles.page}>
+      <Image className={styles.background} src={BackgroundDesktop} alt="background desktop" />
+      <section className={styles.fylo}>
+        <Image src={Logo} alt="logotipo" />
+        <div className={styles.container}>
+          <div className={styles.icon}>
+            <Image src={Document} alt="icon document" />
+          </div>
+          <div className={styles.icon}>
+            <Image src={Folder} alt="icon folder" />
+          </div>
+          <div className={styles.icon}>
+            <Image src={Upload} alt="icon upload" />
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section className={styles.containerArmazenamento}>
+        <p>You’ve used <strong>815 GB</strong> of your storage</p>
+        <div className={styles.slider}>
+          <div className={styles.fundo}>
+            <div className={styles.circle}></div>
+          </div>
+        </div>
+        <div className={styles.armazenamento}>
+          <p>0 GB</p>
+          <p>1000 GB</p>
+        </div>
+        <div className={styles.message}>
+          <p><span>185</span>GB Left</p>
+        </div>
+      </section>
+    </main>
   );
 }
